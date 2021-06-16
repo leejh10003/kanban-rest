5f20f8ed15acfaa01e4440b555a7dae96bfc53cb
Lee, JunHyuk <leejh10003@gmail.com>
2021-06-16 21:15:49 +0900

    Return promise in a function

--- index.js
+++ index.js
@@ -182,16 +182,18 @@ router.post('image', '/image', upload.fields([{
 	try {
 		console.log(ctx.request.files)
 		if (!!(ctx.request.files) && (ctx.request.files.file.length > 0)){
-			const uploads = ctx.request.files.file.map((file) => new Promise(async () => {
-				const fileFromBUffer = await FileType.fromBuffer(file.buffer);
-				const result = await s3.upload({
-					Bucket: S3_BUCKET_NAME,
-					ACL: 'public-read',
-					Body: file.buffer,
-					Key: `/${userId}/${Date.now()}.${fileFromBUffer.ext}`
-				}).promise()
-				return result;
-			}));
+			const uploads = ctx.request.files.file.map((file) => {
+				return new Promise(async () => {
+					const fileFromBUffer = await FileType.fromBuffer(file.buffer);
+					const result = await s3.upload({
+						Bucket: S3_BUCKET_NAME,
+						ACL: 'public-read',
+						Body: file.buffer,
+						Key: `/${userId}/${Date.now()}.${fileFromBUffer.ext}`
+					}).promise()
+					return result;
+				})
+			});
 			console.log(uploads)
 			const uploadResults = await Promise.all(uploads);
 			console.log(uploadResults)
